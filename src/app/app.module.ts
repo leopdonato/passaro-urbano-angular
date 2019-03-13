7c0848206b8f8a8fa3357f6fdb4a6d445a2d2817
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-03-13 17:00:51 -0300

    Inserting pipe currency and creating pipe custom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { registerLocaleData } from "@angular/common";
+import localePt from "@angular/common/locales/pt";
 
 import { ROUTES } from './app.routes';
 
@@ -16,6 +18,9 @@ import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
+//pipe
+import { DescricaoReduzida } from './util/descricao-reduzida.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +31,8 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     DiversaoComponent,
     OfertaComponent,
     ComoUsarComponent,
-    OndeFicaComponent
+    OndeFicaComponent,
+    DescricaoReduzida
   ],
   imports: [
     BrowserModule,
@@ -34,7 +40,9 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'pt-Br'} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+registerLocaleData(localePt);
