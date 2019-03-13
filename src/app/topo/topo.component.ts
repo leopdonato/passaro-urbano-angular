b769f9c5981b256f3dba766377656c2ecd1362a0
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-03-13 15:45:47 -0300

    searchfield with observable finished

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
+import { switchMap, debounceTime, distinctUntilChanged, catchError, onErrorResumeNext } from 'rxjs/operators';
 
 @Component({
   selector: 'xyz-topo',
@@ -12,12 +13,36 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>;
+  public ofertas2: Oferta[];
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-  }
+    this.ofertas = this.subjectPesquisa // retorno Oferta[]
+      .pipe(debounceTime(1000)) //executa a ação do switchMap após 1 segundo
+      .pipe(distinctUntilChanged()) //para fazer pesquisas distintas
+      .pipe(switchMap((termo: string) => {
+        console.log('requisição para api');
 
+        if(termo.trim() === ''){
+          //retornar um obervabele de array de ofertas vazio
+          return of<Oferta[]>([]);
+        }
+        return this.ofertasService.pesquisaOfertas(termo);
+        
+      }))
+    .pipe(catchError((err: any) => {
+        console.log(err);
+        return of<Oferta[]>([]);
+      }));
+
+      this.ofertas.subscribe((ofertas: Oferta[]) => {
+        console.log(ofertas)
+        this.ofertas2 = ofertas
+      })
+  }
+/*
   public pesquisa (termoDaBusca: string): void{
     this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
     
@@ -26,6 +51,13 @@ export class TopoComponent implements OnInit {
       (erro: any) => console.log("Erro Status: ", erro.status),
       () => console.log('Fluxo de eventos completo')
     );
+    
+  }
+  */
+
+  public pesquisa(termoDaBusca: string): void {
+    console.log('keyup caracter: ', termoDaBusca);
+    this.subjectPesquisa.next(termoDaBusca);
   }
 
 }
